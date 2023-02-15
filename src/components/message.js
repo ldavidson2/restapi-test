ac35fd6a37524b5315eab3b961fd48b996ca01ea
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-15 17:21:51 -0600

    added problems back

--- src/components/message.js
+++ src/components/message.js
@@ -1,35 +1,35 @@
-// import React from "react";
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-// import awsmobile from "./aws-exports";
-// import Amplify from "aws-amplify";
-// import { API } from "aws-amplify";
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import awsmobile from "./aws-exports";
+import Amplify from "aws-amplify";
+import { API } from "aws-amplify";
 
-// Amplify.configure(awsmobile);
-// API.configure(awsmobile);
+Amplify.configure(awsmobile);
+API.configure(awsmobile);
 
-// const myAPI = "testapi";
-// const path = "/items";
+const myAPI = "testapi";
+const path = "/items";
 
-// export default function Message() {
-//   const [result, setresult] = useState(null);
-//   const message = async () => {
-//     try {
-//       let res = await API.get(myAPI, path, {});
-//       let result = res.data;
-//       setresult(result);
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
+export default function Message() {
+  const [result, setresult] = useState(null);
+  const message = async () => {
+    try {
+      let res = await API.get(myAPI, path, {});
+      let result = res.data;
+      setresult(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-//   useEffect(() => {
-//     message();
-//   }, []);
-//   return (
-//     <div>
-//       <h1>Results :</h1>
-//       {result}
-//     </div>
-//   );
-// }
+  useEffect(() => {
+    message();
+  }, []);
+  return (
+    <div>
+      <h1>Results :</h1>
+      {result}
+    </div>
+  );
+}
