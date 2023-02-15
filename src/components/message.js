5d9a03fd3aa7b0f47b12b021447d0267e395f021
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-15 16:29:08 -0600

    using api function

--- src/components/message.js
+++ src/components/message.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import 'awsconfig' from './aws-exports';
+import Amplify, { API } from "aws-amplify";
+
+Amplify.configure(awsconfig);
+API.configure(awsconfig);
+
+const myAPI = "testapi";
+const path = "/items";
 
 export default function Message() {
   const [result, setresult] = useState(null);
   const message = async () => {
     try {
-      let res = await axios.get("https://wr564a0aw3.execute-api.us-east-2.amazonaws.com/staging");
+      let res = await API.get(myAPI, path, {});
       let result = res.data;
       setresult(result);
     } catch (e) {
